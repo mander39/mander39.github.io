707e11eb181daf3e52da284ea4e4428d2d62e737
mander39 <39866682+mander39@users.noreply.github.com>
2025-06-15 14:52:16 -0400

    updates

---
+++ nba-decades/script.js
@@ -0,0 +1,59 @@
+$('.hint').click(function() {
+  $(this).toggleClass("show");
+  $("#hint-content").toggleClass("fade-in");
+  $("#content-overlay").toggleClass("hidden");
+});
+
+$('.hint2').click(function() {
+  $(this).toggleClass("show");
+  $("#hint-content2").toggleClass("fade-in");
+  $("#content-overlay2").toggleClass("hidden");
+});
+
+$('.hint3').click(function() {
+  $(this).toggleClass("show");
+  $("#hint-content3").toggleClass("fade-in");
+  $("#content-overlay3").toggleClass("hidden");
+});
+
+$('.hint4').click(function() {
+  $(this).toggleClass("show");
+  $("#hint-content4").toggleClass("fade-in");
+  $("#content-overlay4").toggleClass("hidden");
+});
+
+$('.hint5').click(function() {
+  $(this).toggleClass("show");
+  $("#hint-content5").toggleClass("fade-in");
+  $("#content-overlay5").toggleClass("hidden");
+});
+
+$('.hint6').click(function() {
+  $(this).toggleClass("show");
+  $("#hint-content6").toggleClass("fade-in");
+  $("#content-overlay6").toggleClass("hidden");
+});
+
+$('.hint7').click(function() {
+  $(this).toggleClass("show");
+  $("#hint-content7").toggleClass("fade-in");
+  $("#content-overlay7").toggleClass("hidden");
+});
+
+$('.hint8').click(function() {
+  $(this).toggleClass("show");
+  $("#hint-content8").toggleClass("fade-in");
+  $("#content-overlay8").toggleClass("hidden");
+});
+
+$('.hint9').click(function() {
+  $(this).toggleClass("show");
+  $("#hint-content9").toggleClass("fade-in");
+  $("#content-overlay9").toggleClass("hidden");
+});
+
+$('.hint10').click(function() {
+  $(this).toggleClass("show");
+  $("#hint-content10").toggleClass("fade-in");
+  $("#content-overlay10").toggleClass("hidden");
+});
